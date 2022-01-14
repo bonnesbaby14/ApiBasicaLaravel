cc7031fddacc630d819994f7b1b25a0094d050e1
bonnesbaby14 <bonnesbaby14@gmail.com>
2022-01-13 21:21:46 -0600

    creacion de archivos iniciales para transaciones

---
+++ database/factories/Api/TransactionFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories\api;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TransactionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
